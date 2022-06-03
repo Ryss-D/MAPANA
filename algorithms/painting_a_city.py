eafd1c6fe5ab7875216e682f590a479bccb8d6f5
Ryss-D <ryss-d@protonmail.com>
2022-06-03 15:54:26 -0500

    fix: city pain solution redefined

--- algorithms/painting_a_city.py
+++ algorithms/painting_a_city.py
@@ -1,59 +1,57 @@
 
 matrix = [[6, 2, 6, 6, 2], [6, 1, 2, 7, 3]]
- 
-n = len(matrix)
-m = len(matrix[0])
-side = []
-otherside = []
-front = []
+def paintin_a_city(matrix) :
+    n = len(matrix)
+    m = len(matrix[0])
 
-back = []
-for i in range(m) :
-    x = []
+    floor_rooft = (n * m)*2
+    side = []
+    otherside = []
+    front = []
+    back = []
+
+    for i in range(m) :
+        x = []
+        for line in matrix:
+            x.append(line[i])
+        side.append(x[0])
+        for indexe in range(len(x)-1):
+            if x[indexe] < x[indexe + 1]:
+                side.append(x[indexe+1]-x[indexe])
+            else: 
+                pass
+
+    for i in range(m) :
+        x = []
+        for line in matrix:
+            x.append(line[i])
+        x.reverse()
+        otherside.append(x[0])
+        for indexe in range(len(x)-1):
+            if x[indexe] < x[indexe + 1]:
+                otherside.append(x[indexe+1]-x[indexe])
+            else: 
+                pass
+
     for line in matrix:
-        x.append(line[i])
-    side.append(x[0])
-    for indexe in range(len(x)-1):
-        if x[indexe] < x[indexe + 1]:
-           side.append(x[indexe+1]-x[indexe])
-        else: 
-            pass
+        front.append(line[0])
+        for indexe in range(len(line)-1):
+            if line[indexe] < line[indexe + 1]:
+                front.append(line[indexe+1]-line[indexe])
+            else: 
+                pass
 
-for i in range(m) :
-    x = []
     for line in matrix:
-        x.append(line[i])
-    x.reverse()
-    otherside.append(x[0])
-    for indexe in range(len(x)-1):
-        if x[indexe] < x[indexe + 1]:
-           otherside.append(x[indexe+1]-x[indexe])
-        else: 
-            pass
+        line.reverse()
+        back.append(line[0])
+        for indexe in range(len(line)-1):
+            if line[indexe] < line[indexe + 1]:
+                back.append(line[indexe+1]-line[indexe])
+            else: 
+                pass
 
-for line in matrix:
-    front.append(line[0])
-    for indexe in range(len(line)-1):
-        if line[indexe] < line[indexe + 1]:
-           front.append(line[indexe+1]-line[indexe])
-        else: 
-            pass
+                
 
-for line in matrix:
-    line.reverse()
-    back.append(line[0])
-    for indexe in range(len(line)-1):
-        if line[indexe] < line[indexe + 1]:
-           back.append(line[indexe+1]-line[indexe])
-        else: 
-            pass
+    liters = sum(side) + sum(otherside) + sum(front) + sum(back) + floor_rooft
 
-            
-floor_rooft = (n * m)*2
-
-liters = sum(side) + sum(otherside) + sum(front) + sum(back) + floor_rooft
-print(side)
-print(otherside)
-print(front)
-print(back)
-print(liters)
+    return liters
